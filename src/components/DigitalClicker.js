eeddc0e288ec366d1e1e007dddca40959c3ee1be
Adan Escamilla <adanescamilla580@gmail.com>
2021-03-17 00:37:31 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,46 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       timesClicked: 0,
+    };
+  }
+
+//here we set the timesClicked initial state to 0
+
+  handleClick = () => {
+    this.setState(initialState => ({
+      timesClicked: initialState.timesClicked+1
+    }))
+  }
+//we then create this handle function that uses
+//initialState, then gets the timesClicked and sets it to 
+//the initialState that calls on the timesClicked and adds 1
+//that is then set to timesClicked
+  
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    );
+  }
+//here we create a button that handles a click which 
+//calls on handleClick when clicked
+//the buttons name is calling on timesClicked and will show a number
+
+
+
+
+}
+
+  
+
+
+
+
+
+export default YouTubeDebugger;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,74 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+
+ 
+    constructor() {
+        super();
+    
+        this.state = {
+          errors: [],
+          user: null,
+          settings: {
+            bitrate: 8,
+            video: {
+              resolution: '1080p'
+            }
+          }
+        };
+      }
+      //here we set the initial state 
+
+
+    haandlechangeResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    ...this.state.settings.video,
+                    resolution: '720'
+                }
+            }
+        })
+    }
+    //we made this to alter the state
+    //we first used the setState
+    //then we called on settings and used the spread operator 
+    //to make a new array with the new resolution
+
+    handleChangeBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.setting, 
+                bitrate: 12
+            }
+        })
+    }
+    //we made this to alter the state
+    //we first used the setState
+    //then we called on settings and used the spread operator 
+    //here we updated the bitrate
+
+
+    render() {
+        return (
+            <div>
+                <button className="bitrate" onClick={this.handleChangeBitrate}>
+                    Change Bitrate
+                </button>
+                <button className="resolution" onClick={this.haandlechangeResolution}>
+                    Change Resolution
+                </button>
+            </div>
+        )
+    }
+    //we make two buttons, BOTH with the onClick event
+    //this event will then target the resolution and bitrate
+    //function when clicked and in turn change them from the
+    //initial state
+
+
+}
+
+export default YouTubeDebugger;
